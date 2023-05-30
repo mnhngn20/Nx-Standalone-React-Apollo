1f3c8584c9b43fb38049ec330f524369cc392ec0
Minh Nguyen <mnhngn20@gmail.com>
2023-05-30 14:42:21 +0700

    chore(init): fix auto import

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,15 @@
+import { PostList } from '@lib/features/posts/components';
+import { UserList } from '@lib/features/users/components';
+import { Shared } from '@lib/shared/components';
 import Welcome from './Welcome';
 
 export default function App() {
-  return <Welcome />;
+  return (
+    <>
+      <Welcome />
+      <Shared />
+      <UserList />
+      <PostList />
+    </>
+  );
 }
