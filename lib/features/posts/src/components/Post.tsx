68830f2087224a6cfa2820c2c69e013434383cbe
Minh Nguyen <mnhngn20@gmail.com>
2023-05-31 12:42:40 +0700

    chore(init): fix auto import

---
+++ lib/features/posts/src/components/Post.tsx
@@ -0,0 +1,5 @@
+export function Post() {
+  return <div>Post</div>;
+}
+
+export default Post;
--- lib/features/posts/src/components/PostList.tsx
+++ lib/features/posts/src/components/PostList.tsx
@@ -1,10 +1,12 @@
 import { Shared } from '@lib/shared/components';
+import { Post } from '@lib/features/posts/components';
 
 export function PostList() {
   return (
     <>
       <div>Post List</div>
       <Shared />
+      <Post />
     </>
   );
 }
